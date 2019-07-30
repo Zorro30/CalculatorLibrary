77cd38b6206beff93fe55187ad144527c4af20ed
Gaurang <gaurang.bharatpatel@ltts.com>
2019-07-30 21:01:33 +0530

    Adding CircleCI folder

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 Calculator library containing basic math operations.
 """
 
+
 def add(first_term, second_term):
     return first_term + second_term
+
 
 def subtract(first_term, second_term):
     return first_term - second_term
--- test_calculator.py
+++ test_calculator.py
@@ -6,10 +6,11 @@ import calculator
 
 
 class TestCalculator:
-    
-    
+
+
     def test_addition(self):
         assert 4 == calculator.add(2,2)
+
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4,2)    
